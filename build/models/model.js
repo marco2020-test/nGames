488dc3c25592ca97bc7101bc58bf1b0c5420769a
Carlos Marín Díaz <carlos.marin@trebol-it.com>
2019-06-14 00:42:15 -0400

    Update src/index.ts Deleted build/controllers/controller.js, build/models/clientModel.js, build/models/model.js, build/routes/route.js, build/server.js, src/server.ts files

--- build/models/model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const listas = new mongoose_1.Schema({
-    ID_LIST: Number,
-    DESCRIPTION: String
-});
-exports.default = mongoose_1.model('Post', listas);
