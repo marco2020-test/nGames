9683e2d741be2823782392f534906eecf2c6ecdc
grehisys diaz <grehisys.diaz@trebol-it.com>
2019-07-15 10:50:32 -0400

    ajustes test

--- src/test/client.spec.ts
+++ src/test/client.spec.ts
@@ -2,11 +2,22 @@ import * as routes from '../client/client-routes';
 const nock = require('nock');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+let assert   = require('assert');
 const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:3001/api_admision';
+let models= require ('../client/client-models');
 
-
+describe("Check Sum Function: ", function() {
+ it("Check the returned value using: assert(typeof(value, 'value')): ", function() {
+    let result   = models['codSuper'];
+    assert(typeof(result), "string");
+  });
+  it("Check the returned value using: assert(typeof(value, 'value')): ", function() {
+    let result   = models['codTok'];
+    assert(typeof(result), "string");
+  });
+});
 describe('Admisión', () => {
   describe('Route POST /searchClient', () => {
     it('Ver Admisión', (done) => {
