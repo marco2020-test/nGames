2448ce668a195d50f0d8a75386728a68903f0a36
grehisys diaz <grehisys.diaz@trebol-it.com>
2019-07-17 10:00:19 -0400

    cambios test 4

--- src/test/lists.spec.ts
+++ src/test/lists.spec.ts
@@ -2,9 +2,18 @@ import * as routes from '../lists/lists-route';
 const nock = require('nock');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+let assert = require('assert');
 const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:3001/api_admision';
+let modells = require('../lists/lists-models');
+
+describe("Check Sum Function modells: ", function () {
+  it("Check the returned value using: assert(typeof(value, 'value')): funcion recuperarListas", function () {
+      let result = modells.recuperarListas();
+      assert(typeof (result), "string");
+  });
+});
 
 describe('Admisión', () => {
   describe('Route GET /searchList', () => {
@@ -18,3 +27,4 @@ describe('Admisión', () => {
     });
   });
 });
+
